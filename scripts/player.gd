2a1961d672ea995b3d21d63dc758b528816835cb
Gabe Lapingcao <mgultra2018@gmail.com>
2025-10-26 21:38:04 -0700

    Completed Program 3

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ const MOVEMENT_SPEED := 32.0
 var health := 100
 
 func _physics_process(_delta: float) -> void:
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction != Vector2.ZERO:
 		velocity = direction * MOVEMENT_SPEED
 	else:
