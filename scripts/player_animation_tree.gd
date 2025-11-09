22c6f25c71b956e8b67bcba7b529f8916a3d66a6
Gabe Lapingcao <mgultra2018@gmail.com>
2025-11-09 14:56:39 -0800

    Added attack and health components

--- scripts/player_animation_tree.gd
+++ scripts/player_animation_tree.gd
@@ -7,7 +7,7 @@ extends Node2D
 
 var last_facing_direction := Vector2(0, -1)
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var idle = !player.velocity
 	
 	animation_tree.set("parameters/conditions/idle", idle)
