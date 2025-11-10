b01a1aaf21723d316020b12b54c684dc5c393077
Gabe Lapingcao <mgultra2018@gmail.com>
2025-11-09 22:18:43 -0800

    Implemented victory screen

--- src/levels/test_scene.gd
+++ src/levels/test_scene.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _on_base_health_depleted() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://src/ui/title_screen.tscn")
+	get_tree().call_deferred("change_scene_to_file", "res://src/ui/victory_screen.tscn")
