698b44f7b8e1f416e83fc59b444a8d683e734c5a
Gabe Lapingcao <mgultra2018@gmail.com>
2025-11-09 22:43:18 -0800

    Implemented simple HUD with troop spawn button

---
+++ src/levels/heads_up_display.gd
@@ -0,0 +1,14 @@
+extends Control
+
+# TODO IMPORTANT - refactor this code later to put this logic in the level, NOT the HUD
+@export var spawn_position : Vector2 = Vector2(-274.0, 78.0)
+@export var target : StaticBody2D
+
+@onready var minion = preload("res://src/allies/ally_minion.tscn")
+@onready var main = get_tree().get_root().get_node("TestScene")
+
+func _on_spawn_button_pressed() -> void:
+	var instance = minion.instantiate()
+	instance.global_position = spawn_position
+	instance.target = target
+	main.add_child.call_deferred(instance)
