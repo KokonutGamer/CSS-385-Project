0e36b48372dd55c8d468f7fec9dc48dd4d114cb8
Gabe Lapingcao <mgultra2018@gmail.com>
2025-11-09 20:15:50 -0800

    Refactored naive pathfinding into state design pattern

--- src/allies/ally_minion.gd
+++ src/allies/ally_minion.gd
@@ -10,13 +10,6 @@ var attacking := false
 
 var current_state : State
 
-# TODO remove this function
-# Temporary solution for pathfinding
-func _physics_process(delta: float) -> void:
-	navigation_agent.target_position = target.global_position
-	velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * MOVEMENT_SPEED
-	move_and_slide()
-
 func move_towards(target: Vector2, delta: float) -> void:
 	var dir := target - global_position
 	if dir.length() > 0.01:
--- src/allies/chasing.gd
+++ src/allies/chasing.gd
@@ -2,4 +2,11 @@ extends AllyMinionState
 
 func enter(previous_state_path: String, data := {}) -> void:
 	# TODO chase logic
+	
 	pass
+
+func physics_update(_delta: float) -> void:
+	minion.navigation_agent.target_position = minion.target.global_position
+	minion.velocity = minion.global_position.direction_to(\
+	minion.navigation_agent.get_next_path_position()) * minion.MOVEMENT_SPEED
+	minion.move_and_slide()
