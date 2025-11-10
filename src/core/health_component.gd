2f497238a6f177fcc402d6293accd9f2f838f58f
Gabe Lapingcao <mgultra2018@gmail.com>
2025-11-09 20:54:15 -0800

    Updated health component with more robust getter and setter implementations and signals

--- src/core/health_component.gd
+++ src/core/health_component.gd
@@ -1,16 +1,32 @@
+class_name HealthComponent
 extends Node
 
-class_name HealthComponent
+signal max_health_changed(diff: float)
+signal health_changed(diff: float)
+signal health_depleted
 
-@export var MAX_HEALTH := 10.0
-var health : float
+@export var max_health := 10.0 : set = set_max_health, get = get_max_health
 
-func _ready() -> void:
-	health = MAX_HEALTH
+@onready var health := max_health : set = set_health, get = get_health
+
+func set_max_health(value: float) -> void:
+	# TODO check logic in the future if this needs to have strict checks
+	# i.e. max health limit, checks to restrict to only strict upgrades
+	var diff = max_health - value
+	max_health = value
+	max_health_changed.emit(diff)
 	
-func damage(attack: Attack) -> void:
-	health -= attack.attack_damage
+func get_max_health() -> float:
+	return max_health
 
+func set_health(value: float) -> void:
+	var diff = health - value
+	health = value
+	health_changed.emit(diff)
+	
+	# This entity has died :(
 	if health <= 0:
-		# TODO implement a signal for an entity dying
-		print("This entity has died:", get_parent())
+		health_depleted.emit()
+
+func get_health() -> float:
+	return health
