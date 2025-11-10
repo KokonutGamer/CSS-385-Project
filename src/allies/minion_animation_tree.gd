d957c7c6894fe350a0bf62b80af1efa4ca1ebbca
Gabe Lapingcao <mgultra2018@gmail.com>
2025-11-09 16:47:54 -0800

    Implemented ally minion animation tree and blend space

--- src/allies/minion_animation_tree.gd
+++ src/allies/minion_animation_tree.gd
@@ -2,19 +2,18 @@ extends Node
 
 @export var animation_tree : AnimationTree
 
-# TODO create a type Player
-@onready var player = get_owner()
+@onready var minion = get_owner()
 
 var last_facing_direction := Vector2(0, -1)
 
 func _physics_process(_delta: float) -> void:
-	var idle = !player.velocity
+	var idle = !minion.velocity
 	
 	animation_tree.set("parameters/conditions/idle", idle)
 	animation_tree.set("parameters/conditions/walk", !idle)
 	
 	if !idle:
-		last_facing_direction = player.velocity.normalized()
+		last_facing_direction = minion.velocity.normalized()
 	
 	animation_tree.set("parameters/Walk/blend_position", last_facing_direction)
 	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
