cf0de779fc55b2de55135d58ee375bcb95ed2f71
Gabe Lapingcao <mgultra2018@gmail.com>
2025-11-09 20:08:59 -0800

    Implemented naive solution to pathfinding for ally minions

--- src/allies/ally_minion.gd
+++ src/allies/ally_minion.gd
@@ -1,15 +1,34 @@
 extends CharacterBody2D
 class_name AllyMinion
 
+@onready var navigation_agent : NavigationAgent2D = $NavigationAgent2D
+@export var target : StaticBody2D
+
 const MOVEMENT_SPEED := 28.0
 
 var attacking := false
 
 var current_state : State
 
-func _ready() -> void:
-	pass
-
+# TODO remove this function
+# Temporary solution for pathfinding
 func _physics_process(delta: float) -> void:
-	# TODO implement AStar and attack logic
-	pass
+	navigation_agent.target_position = target.global_position
+	velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * MOVEMENT_SPEED
+	move_and_slide()
+
+func move_towards(target: Vector2, delta: float) -> void:
+	var dir := target - global_position
+	if dir.length() > 0.01:
+		velocity = dir.normalized() * MOVEMENT_SPEED
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
+
+func start_attack() -> void:
+	attacking = true
+	# TODO play attack animation and spawn hitbox
+
+func stop_attack() -> void:
+	attacking = false
+	# TODO stop attack animation and cleanup
