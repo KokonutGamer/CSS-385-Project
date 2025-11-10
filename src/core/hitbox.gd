71b7cd98072457d7947ddcb42267e7d2b759e14b
Gabe Lapingcao <mgultra2018@gmail.com>
2025-11-09 20:35:15 -0800

    Implemented hitbox class

---
+++ src/core/hitbox.gd
@@ -0,0 +1,10 @@
+class_name Hitbox
+extends Area2D
+
+@export var damage: float = 12.0 : set = set_damage, get = get_damage
+
+func set_damage(value: float) -> void:
+	damage = value
+
+func get_damage() -> float:
+	return damage
