deff364a5ed252d67b1b86f223ac097d41ca9a88
Gabe Lapingcao <mgultra2018@gmail.com>
2025-12-10 20:04:10 -0800

    Implemented collision layers for allies and enemy projectiles

--- src/allies/ally_minion.gd
+++ src/allies/ally_minion.gd
@@ -3,6 +3,8 @@ class_name AllyMinion
 
 @onready var navigation_agent : NavigationAgent2D = $NavigationAgent2D
 @onready var health_component : HealthComponent = $HealthComponent
+@onready var hurtbox : Hurtbox = $Hurtbox
+@onready var hitbox : Hitbox = $"Weapon/Hitbox"
 @export var target : StaticBody2D
 
 const MOVEMENT_SPEED := 28.0
@@ -13,6 +15,10 @@ var current_state : State
 
 func _ready() -> void:
 	health_component.health_depleted.connect(queue_free)
+	hitbox.collision_layer = 1
+	hitbox.collision_mask = 1 << 3
+	hurtbox.collision_layer = 1 << 1
+	hurtbox.collision_mask = 1 << 2
 
 func move_towards(move_target: Vector2, _delta: float) -> void:
 	var dir := move_target - global_position
--- src/towers/projectile.gd
+++ src/towers/projectile.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var hitbox : Hitbox = $Hitbox
+@onready var hurtbox : Hurtbox = $Hurtbox
 @export var SPEED = 64
 
 var dir : float
@@ -9,6 +11,10 @@ var spawn_rotation : float
 func _ready() -> void:
 	global_position = spawn_position
 	global_rotation = spawn_rotation
+	hitbox.collision_layer = 1 << 2
+	hitbox.collision_mask = 1 << 1 # detect ally hurtbox
+	hurtbox.collision_layer = 1 << 3
+	hurtbox.collision_mask = 1 << 1 # detect ally hurtbox (unique to this case)
 	
 func _physics_process(_delta: float) -> void:
 	velocity = Vector2(0, -SPEED).rotated(deg_to_rad(dir))
